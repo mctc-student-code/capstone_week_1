c3b4ab477340668f36da8f859371050116dea12d
minleg <minleg@minleg.com>
2023-01-09 22:24:07 -0600

    improved version

--- week1/warm_up2.py
+++ week1/warm_up2.py
@@ -2,7 +2,7 @@ def main():
     number_of_classes = input('How many classes are you taking this semester? ')
     class_list  = []
     for number in range(int(number_of_classes)):
-        class_name = input('Enter the name of the class: ')
+        class_name = input('Enter the name of the class number ' + str(number+1) + ': ')
         class_list.append(class_name)
     
     for index,name in enumerate(class_list):
