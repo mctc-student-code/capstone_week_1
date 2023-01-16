3a58062f86d725fb45301ad80719be8455ee8984
minleg <minleg@minleg.com>
2023-01-15 22:29:31 -0600

    test - add new sports option

--- project-1-quiz-refactor-Minleg-main/my_quiz_version.py
+++ project-1-quiz-refactor-Minleg-main/my_quiz_version.py
@@ -12,28 +12,42 @@ def main():
     # dictionary to store questions and answers as key value pairs
     art_questions_answer = {'Who painted the Mona Lisa?': 'Leonardo Da Vinci', 
                             'What precious stone is used to make the artist\'s pigment ultramarine?': 'Lapiz lazuli',
-                            'Anish Kapoor\'s bean-shaped Cloud Gate sculpture is a landmark of which city?': 'Chicago'}
+                            'Anish Kapoor\'s bean-shaped Cloud Gate sculpture is a landmark of which city?': 'Chicago',
+                            'Which kid\'s TV characters are named after Renaissance artists?': 'Teenage Mutant Ninja Turtles',
+                            'The graphite in an artist\'s pencil is made of what chemical element?': 'Carbon'}
 
     space_questions_answer = {'Which planet is closest to the sun?': 'Mercury',
                               'Which planet spins in the opposite direction to all the others in the solar system?': 'Venus',
-                              'How many moons does Mars have?': '2'}
+                              'How many moons does Mars have?': '2',
+                              'What was the first human-made object to leave the solar system?': 'Voyager 1',
+                              'When an asteroid enters the Earth\'s atmosphere and burns up, it is known as what?': 'Meteor'}
+    
+    sports_questions_answer = {'Which gymnast is the "triple-twisting double-tucked salto backwards" skill named after?': 'Simone Biles',
+                               'Which country has won the soccer world cup the most times?': 'Brasil',
+                               'What does MLB stand for?' : 'Major League Baseball'}
     
     number_of_art_questions = len(art_questions_answer) # gets the number of questions for arts topic
     number_of_space_questions = len(space_questions_answer) # gets the number of questions for space topic
+    number_of_sports_questions = len(sports_questions_answer)
 
     if topic == 'art': 
         total = questions(art_questions_answer) # user is asked the questions on the topic and their result is stored in total variable
         output(topic,total,number_of_art_questions) # output is printed
-    else : 
+    elif topic == 'space': 
         total = questions(space_questions_answer) # questions asked on space topic and result is obtained in total
         output(topic,total,number_of_space_questions) # their quiz result is printed
+    else :
+        total = questions(sports_questions_answer)
+        output(topic,total,number_of_sports_questions)
 
 def chooseGenre(): # method for user to choose the topic for quiz, either art or space
-    genre = input("Would you like art, or space questions ? : ")
+    genre = input("Would you like art, space, or sport questions ? : ")
     if(genre.lower() == 'art'):
         return 'art'
     elif(genre.lower() == 'space'):
         return 'space'
+    elif(genre.lower() == 'sport'):
+        return 'sport'
     else:
         return chooseGenre() # if user enters an invalid options, the questions is asked again until user selects a valid option
 
