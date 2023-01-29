9615ecba1eb50e6e280acbe79884240aad2a64dc
minleg <minleg@minleg.com>
2023-01-29 16:13:19 -0600

    area testing

---
+++ week4/AreaTest/area.py
@@ -0,0 +1,11 @@
+"""
+Functions for computing areas of various shapes
+"""
+
+def triangle_area(height, base):
+    if base < 0 or height < 0:
+        raise ValueError('Base and height must be 0 or positive')
+    return height * base * 0.5
+
+def square_area(height, base):
+    return height * base
---
+++ week4/AreaTest/test_area.py
@@ -0,0 +1,38 @@
+"""
+To run the test without the unittest main method, on terminal run  python3 -m unittest test_area.py
+
+all test function should start their name with word test
+
+if the assertion method is not written in the test function, the test is considered to be passed which could be a source of error
+"""
+from unittest import TestCase
+import area
+
+class TestShapeAreas(TestCase):
+    
+    def test_triangle_area(self):
+        # A triangle with height 4 and base 5 shold have area 10
+        self.assertEqual(10, area.triangle_area(4,5))
+    
+    def test_triangle_area_floating_point(self):
+        self.assertAlmostEqual(17.79875, area.triangle_area(7.25, 4.91)) # to avoid the small fractional error - rounding
+        
+    # Tests if either height or base is zero or both is zero
+    def test_triangle_zero_height_base(self):
+        self.assertEqual(0, area.triangle_area(0,5))
+        self.assertEqual(0, area.triangle_area(5,0))
+        self.assertEqual(0, area.triangle_area(0,0))
+    
+    # Tests for negative height or base or both are negative
+    def test_negative_base_height_raises_value_error(self):
+        with self.assertRaises(ValueError):
+            area.triangle_area(-9,10)
+        
+        with self.assertRaises(ValueError):
+            area.triangle_area(10,-9)
+        
+        with self.assertRaises(ValueError):
+            area.triangle_area(-9,-10)
+        
+    def test_square_area(self):
+        self.assertEqual(20, area.square_area(4,5))
